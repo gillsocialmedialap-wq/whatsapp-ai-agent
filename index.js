f96189624722da0cd6343e33069e9853a9c28eed
gillsocialmedialap-wq <gillsocialmedialap@gmail.com>
2026-07-07 03:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const axios = require("axios");
-
+const path = require("path");
 const app = express();
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.send("Hi Buzzy AI is LIVE 🚀");
 });
